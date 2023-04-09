3ddc3b54c03414d6ae0e42ef99494b96aafdf6bf
RamiroZabala <ramirozabala5@gmail.com>
2023-04-09 04:45:02 -0300

    Añadio OPTIONS a Access-Control-Allow-Methods para login 2

--- src/main/java/com/portfolio/portfolio/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/portfolio/portfolio/security/config/JwtAuthenticationFilter.java
@@ -33,7 +33,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ///////// VA????
     //response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
     //response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Origin", "https://portfolioweb-argprog.web.app");
+    response.setHeader("Access-Control-Allow-Origin", "https://portfolioweb-argprog.web.app/");
     response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     response.setHeader("Access-Control-Allow-Header", "*");
     ////////////////
